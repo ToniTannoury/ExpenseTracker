b09f683f2f77682366bac00d7e5556cddf97ffff
ToniTannoury <tatannoury01@gmail.com>
2023-07-19 11:15:13 +0300

    Adding the saveExpenses functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   let expenses = []
+  let savedExpenses = []
   $('#addExpense').on('click', function() {
     let expenseName = $('#expenseName').val()
     let expenseAmount = parseFloat($('#expenseAmount').val())
@@ -47,6 +48,34 @@ $(document).ready(function() {
     }, 0)
 
     $('#totalExpenses').text('$' + total.toFixed(2))
+  }
+
+  $('#saveExpenses').on('click', function() {
+    saveExpenses()
+  })
+
+  function saveExpenses() {
+    savedExpenses.push(...expenses)
+    if (expenses.length === 0) {
+      alert('There are no expenses to save.')
+      return
+    }
+    expenses = []
+  
+    $('#expenseTable tbody').empty()
+  
+    // Reset total expenses to 0
+    updateTotalExpenses(0)
+  
+    let spentOn = savedExpenses
+    for (let i = 0; i < spentOn.length; i++) {
+      let savedExpense = spentOn[i]
+      let listItem = '<li>' + savedExpense.name + ': $' + savedExpense.amount.toFixed(2) + ' (Created at: ' + savedExpense.created_at + ')</li>'
+      $('#saved-expense-list').append(listItem)
+    }
+    spentOn = []
+    console.log(savedExpenses)
+    alert('Expenses saved successfully!')
   }
 
 
