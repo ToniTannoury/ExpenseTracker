3b8e7308c63ed381bbcbda196cf0c4a8777140ae
ToniTannoury <tatannoury01@gmail.com>
2023-07-19 11:07:05 +0300

    Adding buttons for saving the lists of expenses

--- script.js
+++ script.js
@@ -16,6 +16,34 @@ $(document).ready(function() {
     console.log(expenses)
     $('#expenseName').val('')
     $('#expenseAmount').val('')
+    updateExpenseTable()
   })
-  
+  function updateExpenseTable() {
+    let tableBody = $('#expenseTable tbody')
+    tableBody.empty()
+
+    for (let i = 0; i < expenses.length; i++) {
+      let expense = expenses[i]
+      let row = '<tr>' +
+        '<td>' + expense.name + '</td>' +
+        '<td>$' + expense.amount.toFixed(2) + '</td>' +
+        '<td><button class="deleteExpense" data-index="' + i + '">Delete</button></td>' +
+        '</tr>'
+      tableBody.append(row)
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
